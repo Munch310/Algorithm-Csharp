f6e7c2c90022948b58857edc93918bbbd7638674
문현우 <84183932+Munch310@users.noreply.github.com>
2023-09-05 09:19:56 +0900

    Update 없는 숫자 더하기.cs

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -1,12 +1,14 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-
+using System.Collections.Generic;
 
 public class Solution {
     public int solution(int[] numbers) {
-        var numberArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            return numberArray.Except(numbers).Sum();
+        int[] compare = {1,2,3,4,5,6,7,8,9,0};
+        IEnumerable<int> ret = compare.Except(numbers);
+        int answer = 0;
+        foreach (int num in ret)
+            answer += num;
+        return answer;
     }
 }
