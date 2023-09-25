7404fa2fabf00f6f56cff9e0f6a57fe8fc8ec6d9
문현우 <84183932+Munch310@users.noreply.github.com>
2023-09-25 10:53:11 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 2.10 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(int[] food) {
+        string answer = "";
+        string str = "";
+        for (int i = 0; i < food.Length; i++) {
+            for(int j = 0; j < food[i] / 2; j++){
+                answer += i.ToString();
+            }
+        }
+        str = string.Concat(answer.Reverse());
+        answer += "0" + str;
+        return answer;
+    }
+}
