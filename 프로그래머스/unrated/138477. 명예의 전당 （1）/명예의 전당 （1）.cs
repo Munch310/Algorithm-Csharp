5c3a5795f5d33a14ecd56580360b12344a806951
문현우 <84183932+Munch310@users.noreply.github.com>
2023-09-27 10:00:02 +0900

    [unrated] Title: 명예의 전당 (1), Time: 1.84 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.Length];
+        List<int> honor= new List<int>();
+        for(int i = 0; i < score.Length; i++)
+        {
+            if(honor.Count == k)
+            {
+                if(honor[0] > score[i])
+                {
+                    answer[i] = honor[0];
+                    continue;
+                }
+                honor.RemoveAt(0);
+            }
+            honor.Add(score[i]);
+            honor.Sort();
+            answer[i] = honor[0];
+        }
+        return answer;
+    }
+}
