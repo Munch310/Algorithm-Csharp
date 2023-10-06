92081e70d05aa47900ef9f84545b9d8558bd2fcf
문현우 <84183932+Munch310@users.noreply.github.com>
2023-10-06 09:48:46 +0900

    Update 모의고사.cs

--- 프로그래머스/lv1/42840. 모의고사/모의고사.cs
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.cs
@@ -5,41 +5,34 @@ using System.Linq;
 public class Solution {
     public int[] solution(int[] answers) {
         List<int> answer = new List<int>();
-        int[] num1 = new int[5] {1,2,3,4,5};
-        int[] num2 = new int[8] {2,1,2,3,2,4,2,5};
-        int[] num3 = new int[10] {3,3,1,1,2,2,4,4,5,5};
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
+        int[][] patterns = new int[3][]{
+            new int[] {1,2,3,4,5},
+            new int[] {2,1,2,3,2,4,2,5},
+            new int[] {3,3,1,1,2,2,4,4,5,5}
+        };
+        int[] counts = new int[3];
         
         for(int i = 0; i < answers.Length; i++){
-            if(answers[i] == num1[i % num1.Length]){
-                count1++;
-            }
-            if(answers[i] == num2[i % num2.Length]){
-                count2++;
-            }
-            if(answers[i] == num3[i % num3.Length]){
-                count3++;
+            for(int j = 0; j< patterns.Length; j++){
+                if(answers[i] == patterns[j][i % patterns[j].Length]){
+                    counts[j]++;
+                }
             }
         }
         
-        Dictionary<int,int> counts=new Dictionary<int,int>(){
-            {1,count1},
-            {2,count2},
-            {3,count3}
-          };
-          
-          var maxCount=counts.Values.Max();
-          
-          foreach (var item in counts)
-          {
-              if (item.Value == maxCount)
-              {
-                  answer.Add(item.Key);
-              }
-           }
-
-           return answer.ToArray();
-       }
+        Dictionary<int, int> countsDic = new Dictionary<int, int>();
+        
+        for(int i = 0; i< counts.Length; i++){
+            countsDic.Add(i + 1, counts[i]);
+        }
+        
+        var maxCount = countsDic.Values.Max();
+        
+        foreach(var item in countsDic){
+            if(item.Value == maxCount){
+                answer.Add(item.Key);
+            }
+        }
+        return answer.ToArray();
+    }
 }
