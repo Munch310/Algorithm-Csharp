9c9450750e7d278ba788a096481bfc98576d9e4c
문현우 <84183932+Munch310@users.noreply.github.com>
2023-10-10 09:47:28 +0900

    [level 1] Title: 소수 만들기, Time: 3.55 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Solution
+{
+    public int solution(int[] nums)
+    {
+        int answer = 0;
+        for(int i = 0; i < nums.Length - 2; i++){
+            for(int j = i + 1; j < nums.Length - 1; j++){
+                for(int k = j + 1; k < nums.Length; k++){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    bool isPrime = true;
+                    if(sum <= 1){
+                        isPrime = false;
+                    }
+                    else{
+                        for(int p = 2, sq=(int)Math.Sqrt(sum); p<=sq; ++p){
+                            if(sum % p == 0){
+                                isPrime = false;
+                                break;
+                            }
+                        }
+                    }
+                    if(isPrime){
+                        answer++;
+                    }
+                }
+            }
+            
+        }
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.Console.WriteLine(answer);
+
+        return answer;
+    }
+}
