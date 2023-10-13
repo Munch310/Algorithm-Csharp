d55ebd5998d3e400dc4b76f1805218276ef6cda0
문현우 <84183932+Munch310@users.noreply.github.com>
2023-10-13 14:39:07 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 2.05 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
@@ -5,9 +5,9 @@ public class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] rank = {6, 6, 5, 4, 3, 2, 1};
 
-        int minRank = lottos.Count(n => n == 0);
-        int maxRank = lottos.Count(n => win_nums.Contains(n));
+        int firstRank = lottos.Count(n => n == 0);
+        int lastRank = lottos.Count(n => win_nums.Contains(n));
 
-        return new [] {rank[minRank + maxRank], rank[maxRank]};
+        return new [] {rank[firstRank + lastRank], rank[lastRank]};
     }
 }
