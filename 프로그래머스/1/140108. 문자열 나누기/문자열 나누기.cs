a672ce6051564f4c4b29f7e3d594b06dfeea7cad
문현우 <84183932+Munch310@users.noreply.github.com>
2023-10-19 09:25:36 +0900

    [level 1] Title: 문자열 나누기, Time: 0.27 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Solution {
+    public int solution(string s) {
+        int count = 0;
+        int xCount = 0;
+        int notXCount = 0;
+        
+        char x = s[0];
+        
+        for(int i = 0; i < s.Length; i++){
+            if(s[i] == x)
+                xCount++;
+            else
+                notXCount++;
+            if(xCount == notXCount){
+                count++;
+                if(i + 1 < s.Length)
+                    x = s[i+1];
+                xCount = 0;
+                notXCount = 0;
+            }
+        }
+        
+        if(xCount != notXCount && (xCount > 0 || notXCount > 0))
+            count++;
+        
+        return count;
+    }
+}
